387f3829ea6960ebb3cfb2a6d8410e5ba17c3251
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-03-06 15:09:00 +0100

    dfd

---
+++ fri_challenge.py
@@ -0,0 +1,15 @@
+def pyramid(num):
+    for a in range(num):
+        print(a*"#")
+        
+pyramid(6)        
+
+
+
+def pyramid1(num):
+    for a in range(num):
+        for b in range(a+1):
+            print("#", end = "")
+        print("\n")        
+             
+pyramid1(6)             
