0e76c98f1422117c879b4c8b852f352fad55d79e
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-04-19 23:53:25 +0200

    standarsclaer applied

--- Chapter 02/07. Gradient Boosting and Encoding/train.py
+++ Chapter 02/07. Gradient Boosting and Encoding/train.py
@@ -3,14 +3,17 @@ import numpy
 x_train, x_test, y_train, y_test = dh.get_data("Chapter 02/07. Gradient Boosting and Encoding/insurance.csv")
 
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
 clf = RandomForestRegressor(n_estimators=100, criterion='mae')
 
 
+x_train_scaled = scaler.fit_transform(x_train,y_train)
+x_test_scaled = scaler.transform(x_test)
 
-
-clf.fit(x_train, y_train)
+clf.fit(x_train_scaled, y_train)
 # make predictions
-predictions = clf.predict(x_test)
+predictions = clf.predict(x_test_scaled)
 scores = (predictions == y_test).sum()/len(y_test)
 print(scores)
 print(dh.hello)
