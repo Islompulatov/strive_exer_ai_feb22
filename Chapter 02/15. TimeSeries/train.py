03e75d284fb3cccd73df55c564dfffd7f7240a1c
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-05-06 16:27:08 +0200

    model has applied for time series data

--- Chapter 02/15. TimeSeries/train.py
+++ Chapter 02/15. TimeSeries/train.py
@@ -1,5 +1,9 @@
 import pandas as pd 
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.preprocessing import StandardScaler
+from sklearn import metrics
 data=pd.read_csv("Chapter 02/15. TimeSeries/climate.csv")
 data = data.drop(["Date Time"], axis = 1)
 
@@ -33,10 +37,10 @@ print(y[2])
 
 
 
-def getfeatures(data):
+def new_features(data):
 
     
-    new_data = []
+    new_feat = []
     
     for i in range(data.shape[0]):
 
@@ -44,13 +48,24 @@ def getfeatures(data):
     
         for j in range(data.shape[2]):
 
-            group.append(np.mean(data[i][:, j]))  
-            group.append(np.std(data[i][:, j]))
+            group.append(np.std(data[i][:, j]))  
+            group.append(np.mean(data[i][:, j]))
             group.append(data[i][:, j][-1])     
 
-        new_data.append(group)
+        new_feat.append(group)
+    return np.array(new_feat)
+    
+        
 
-    return np.array(new_data)
+x = new_features(x)
+print(x.shape)
 
 
-new_x = getfeatures(x)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+model = RandomForestRegressor()
+model.fit(x_train,y_train)
+pred = model.predict(x_test)
+scores = cross_val_score(model, x_train,y_train, cv = 3)
+print(scores)
