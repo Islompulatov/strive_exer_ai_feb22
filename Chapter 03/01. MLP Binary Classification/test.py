0673f2a68201ff843298146f59b53a734dd7cabe
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-05-24 11:53:39 +0200

    Updated wtih different functions

--- Chapter 03/01. MLP Binary Classification/test.py
+++ Chapter 03/01. MLP Binary Classification/test.py
@@ -13,12 +13,14 @@ X = torch.tensor(data.drop(2, axis=1).values, dtype=torch.float)
 y = torch.tensor(data[1].values, dtype=torch.float).reshape(-1,1)
 
 class NeuralNetwork(nn.Module):
-    def __init__(self, input_dim, num_hidden1, num_hidden2):
+    def __init__(self, input_dim, num_hidden1, num_hidden2, num_hidden3):
         super().__init__()
         self.fc1 = nn.Linear(input_dim, num_hidden1)
         self.fc2 = nn.Linear(num_hidden1, num_hidden2)
-        self.fc3 = nn.Linear(num_hidden2, 1)
+        self.fc3 = nn.Linear(num_hidden2,num_hidden3)
+        self.fc4 = nn.Linear(num_hidden3, 1)
         self.sigmoid = nn.Sigmoid()
+        self.logsigmoid = nn.LogSigmoid()
     
     def forward(self, x):
         layer1 = self.fc1(x)
@@ -26,15 +28,17 @@ class NeuralNetwork(nn.Module):
         layer2 = self.fc2(act1)
         act2 = self.sigmoid(layer2)
         layer3 = self.fc3(act2)
-        out = self.sigmoid(layer3)
+        act3 = self.logsigmoid(layer3)
+        layer4 = self.fc4(act3)
+        out = self.sigmoid(layer4)
         return out
 
 
-model = NeuralNetwork(X.shape[1], num_hidden1=5, num_hidden2=6)
+model = NeuralNetwork(X.shape[1], num_hidden1=5, num_hidden2=6, num_hidden3=4)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01) 
-loss = nn.BCELoss()       
+loss = nn.BCELoss()     
 loss_func = []
-epochs = 100
+epochs = 500
 for epoch in range(epochs):
 
     pred = model(X)
@@ -42,7 +46,8 @@ for epoch in range(epochs):
     ls.backward()
     optimizer.step()
     optimizer.zero_grad()
-    loss_func.append(ls.detach())
+    if epoch % 2 == 0:
+        loss_func.append(ls.detach())
 print(loss_func)
 plt.plot(loss_func)  
 plt.show()  
