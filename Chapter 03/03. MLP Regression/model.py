486a9a6ba32f345433ea685a7b576a923d948a5c
Islompulatov <98391405+Islompulatov@users.noreply.github.com>
2022-05-27 16:48:06 +0200

    some models trained

--- Chapter 03/03. MLP Regression/model.py
+++ Chapter 03/03. MLP Regression/model.py
@@ -4,6 +4,21 @@ from torch import nn
 from torchsummary import summary
 
 # YOUR CODE HERE
+class Net(nn.Module):
+   def __init__(self,input_size, hidden_layer1, hidden_layer2, output_size):
+       super(Net, self).__init__()
+       self.fc1 = nn.Linear(input_size, hidden_layer1)
+       self.fc2 = nn.Linear(hidden_layer1, hidden_layer2)
+       self.fc3 = nn.Linear(input_size, output_size)
+
+   def forward(self, x):
+       layer1 = self.fc1(x)
+       layer2 = self.fc2(layer1)
+       output = self.fc3(layer2)
+
+       return output
+
+model = Net()
 
 
 
